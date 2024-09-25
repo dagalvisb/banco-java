09f2bb996786ae8290bd6d850515fc54ae4f2080
dagalvisb <daniel.galvis42587@ucaldas.edu.co>
2024-09-25 18:29:46 -0500

    edit code

--- Cuenta.java
+++ Cuenta.java
@@ -1,16 +1,47 @@
 public class Cuenta {
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     String numero;
     String tipo;
     double saldo;
 
-    /**
+    
     public Cuenta (String numero, String tipo, double saldo){
         this.numero = numero;
         this.tipo = tipo;
         this.saldo = saldo;
     }
-    */
+
+    public Cuenta (String numero, String tipo){
+        this(numero, tipo, 0);
+    }
+
+    public Cuenta(){
+        this("", "Ahorros", 0);
+    }
 
     public boolean retirarSaldo(double cantidadARetirar){
         if (cantidadARetirar <= this.saldo){
--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     
     public static void main(String[] args) {
-        int valor = 100000;
-        Cuenta cuenta = new Cuenta();
+        /**int valor = 100000;
+        Cuenta cuenta = new Cuenta("059-908-548-17", "ahorros", 100000 );
         System.out.println(cuenta.saldo);
         cuenta.consignarSaldo(valor);
         System.out.println(cuenta.saldo);
@@ -13,6 +15,48 @@ public class Main {
         boolean pudoSegundoRetiro = cuenta.retirarSaldo(50000.5);
         System.out.println("Pudo Reitat: " + pudoSegundoRetiro);
         System.out.println(cuenta.consultarSaldo());
+
+        Cuenta cuentaNueva = new Cuenta("12345-1", "corriente");
+        System.out.println(cuentaNueva.getNumero());
+        cuentaNueva.setNumero("1467-2");
+        System.out.println(cuentaNueva.getNumero());
+        */
+
+        Cuenta cuenta1 = new Cuenta("1234-6", "Corriente", 100000);
+        Cuenta cuenta2 = new Cuenta("1234-8", "Ahorros");
+        Cuenta cuenta3 = cuenta1;
+
+        ArrayList<Cuenta> cuentas = new ArrayList<>();
+
+        cuentas.add(cuenta1);
+        cuentas.add(cuenta2);
+        cuentas.add(cuenta3);
+
+        //System.out.println(cuentas.get(0).consultarSaldo());
+
+        /**for (int i = 0; i < cuentas.size(); i++){
+            
+            System.out.printf("Numero de cuenta: %s, Tipo de cuenta: %s, El saldo es: %.2f \n", cuentas.get(i).getNumero(), cuentas.get(i).getTipo(), cuentas.get(i).getSaldo());
+        }
+        */
+
+        System.out.println("____________________________________________________________");
+        
+        for (Cuenta cuenta : cuentas) {
+            System.out.printf("Numero de cuenta: %s, Tipo de cuenta: %s, El saldo es: %.2f \n", cuenta.getNumero(), cuenta.getTipo(), cuenta.getSaldo());
+        }
+
+        /**System.out.println("____________________________________________________________");
+
+        cuentas.forEach(cuenta -> {
+            System.out.printf("Numero de cuenta: %s, Tipo de cuenta: %s, El saldo es: %.2f \n", cuenta.getNumero(), cuenta.getTipo(), cuenta.getSaldo());
+        });
+        */
+        double resultado = 0;
+        for (Cuenta cuenta : cuentas) {
+            resultado += cuenta.getSaldo();
+        }
+        System.out.println("resultado es: " + resultado);
     }
 
 }
